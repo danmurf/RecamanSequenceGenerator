779fe62e9be5f328d930cfe8c47f934be2f1d963
Dan Murfitt <dan@murfitt.net>
2018-12-13 19:07:58 +0000

    It generates numbers in the Recamán Sequence.

--- src/RecamanSequenceGenerator.php
+++ src/RecamanSequenceGenerator.php
@@ -4,8 +4,32 @@ namespace danmurf\RecamanSequence;
 
 class RecamanSequenceGenerator
 {
-    public function __invoke()
+    /** @var int */
+    private $position = 0;
+
+    /** @var int */
+    private $hops = 0;
+
+    /** @var array */
+    private $numbersYielded = [];
+
+    public function __invoke(): \Generator
     {
-        yield;
+        do {
+            $backwardNumber = $this->position - $this->hops;
+            if ($backwardNumber >= 0 && !\in_array($backwardNumber, $this->numbersYielded)) {
+                yield $backwardNumber;
+                $this->numbersYielded[] = $backwardNumber;
+                ++$this->hops;
+                $this->position = $backwardNumber;
+            } else {
+                $forwardNumber = $this->position + $this->hops;
+                yield $forwardNumber;
+                $this->numbersYielded[] = $forwardNumber;
+
+                ++$this->hops;
+                $this->position = $forwardNumber;
+            }
+        } while (true);
     }
 }
